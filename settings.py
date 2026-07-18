bb58df2c4e3ac4bd9b96227eb01a5d9acfca6007
SIRSATH SAMEER SAHEBRAO <sameersirsath2503@outlook.com>
2026-07-18 11:39:49 +0530

    Add SSL support for cloud MySQL (PlanetScale)

--- settings.py
+++ settings.py
@@ -7,5 +7,9 @@ DB_CONFIG = {
     'host': os.getenv('DB_HOST', 'localhost'),
     'database': os.getenv('DB_NAME', 'medical_equipment'),
     'port': int(os.getenv('DB_PORT', 3306)),
-    'charset': 'utf8mb4'
+    'charset': 'utf8mb4',
+    'ssl': {'ssl': {}} if os.getenv('DB_SSL', 'false').lower() == 'true' else None,
+    'connect_timeout': 10,
 }
+# Remove None ssl key
+DB_CONFIG = {k: v for k, v in DB_CONFIG.items() if v is not None}
